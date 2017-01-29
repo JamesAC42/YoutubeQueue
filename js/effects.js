bedad0d51f26638ae012d7f543a97921bb92a268
James Crovo <jamescrovo2019@lschs.org>
2017-01-29 02:14:14 -0500

    Redesign style, add tabs for future search capability

--- js/effects.js
+++ js/effects.js
@@ -1,6 +1,9 @@
+//Initialize variables
 var moused;
 var old_spot;
+
 $(document).ready(function(){
+	
 	$(".action-left").mouseenter(function(){
 		var inner_elem = $(this).children('.action-left-inner');
 		moused = inner_elem.text();
@@ -9,14 +12,17 @@ $(document).ready(function(){
 		var inner_elem = $(this).children('.action-left-inner');
 		inner_elem.text(moused).removeClass('action-left-over');
 	});
+
 	$(".action-left").resizable({
 		autoHide:true
 	});
+
 	$(".action-right").mouseenter(function(){
 		$(this).children(".action-right-inner").addClass("action-right-inner-over");
 	}).mouseleave(function(){
 		$(this).children(".action-right-inner-over").removeClass("action-right-inner-over");
 	});
+
 	$(".vid-list").sortable({
 		axis: "y",
 		handle:".action-right-inner",
@@ -28,6 +34,7 @@ $(document).ready(function(){
 			angular.element($('#vid-container')).scope().reSort(old_spot, ui.item.attr('id'));
 		}
 	});
+
 	$(".remove-all-btn").mouseenter(function(){
 		$(this).animate({
 			"width":"+=75px"
@@ -36,9 +43,13 @@ $(document).ready(function(){
 		$(this).animate({
 			"width":"-=75px"
 		},"fast").text("X");
-	}).mousedown(function(){
-		$(this).css("top","1.2em");
-	}).mouseup(function(){
-		$(this).css("top","1em");
+	});
+
+	$(".menu-item").click(function(){
+		if(!$(this).hasClass("menu-item-active")){
+			$(".menu-item-active").removeClass("menu-item-active");
+			$(this).addClass("menu-item-active");
+			$(".window").toggleClass("window-inactive");
+		}
 	})
 });
